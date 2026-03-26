a68d77bbd083307ffa9d056e1e969ca6e8498c71
DexZed <144045080+DexZed@users.noreply.github.com>
2026-03-26 20:38:02 +0600

    `Added Skeleton component`

---
+++ macrohard/src/components/Skeleton.jsx
@@ -0,0 +1,13 @@
+function Skeleton() {
+  return (
+    <>
+      <section className="min-h-screen flex-centered-y">
+        <span className="font-bold text-6xl skeleton skeleton-text">
+          Loading...
+        </span>
+      </section>
+    </>
+  );
+}
+
+export default Skeleton;
