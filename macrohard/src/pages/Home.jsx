26827766054cde2fe10023271fb006726910246f
DexZed <144045080+DexZed@users.noreply.github.com>
2026-03-26 20:48:01 +0600

    `Added Home page components: Hero, Stats, and CatalogueHome`

---
+++ macrohard/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import CatalogueHome from "../components/CatalogueHome";
+import Hero from "../components/Hero";
+import Stats from "../components/Stats";
+
+function Home() {
+  return (
+    <>
+      <Hero />
+      <Stats />
+      <CatalogueHome />
+    </>
+  );
+}
+
+export default Home;
