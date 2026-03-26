f0b245fc5bb38ccabc3f62d8526d00ae558b181c
DexZed <144045080+DexZed@users.noreply.github.com>
2026-03-26 20:56:00 +0600

    `Refactor App component to use React Router and lazy loading`

--- macrohard/src/App.jsx
+++ macrohard/src/App.jsx
@@ -1,121 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { Route, Routes, useLocation } from "react-router";
+import "./App.css";
+import AppContextProvider from "./store/State";
+import { AnimatePresence } from "motion/react";
+import PageTransition from "./components/PageTransition";
+import { lazy, Suspense } from "react";
+import Skeleton from "./components/Skeleton";
+import { ToastContainer } from "react-toastify";
+import Nav from "./components/Nav.jsx";
+import Footer from "./components/Footer.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Home = lazy(() => import("./pages/Home.jsx"));
+const Catalogue = lazy(() => import("./pages/Catalogue.jsx"));
+const Installations = lazy(() => import("./pages/Installations.jsx"));
+const Details = lazy(() => import("./pages/Details.jsx"));
+const ErrorPage = lazy(() => import("./pages/ErrorPage.jsx"));
 
+function Container({ children }) {
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      <Suspense fallback={<Skeleton />}>
+        <PageTransition>
+          <Nav />
+          {children}
+          <Footer />
+        </PageTransition>
+      </Suspense>
     </>
-  )
+  );
 }
 
-export default App
+function App() {
+  const location = useLocation();
+  return (
+    <>
+      <main>
+        <ToastContainer />
+        <AppContextProvider>
+          <AnimatePresence mode="wait">
+            <Routes location={location} key={location.pathname}>
+              <Route
+                index
+                element={
+                  <Container>
+                    <Home />
+                  </Container>
+                }
+              />
+              <Route
+                path="/apps"
+                element={
+                  <Container>
+                    <Catalogue />
+                  </Container>
+                }
+              />
+              <Route
+                path="/installation"
+                element={
+                  <Container>
+                    <Installations />
+                  </Container>
+                }
+              />
+              <Route
+                path="/detail/:id"
+                element={
+                  <Container>
+                    <Details />
+                  </Container>
+                }
+              />
+              <Route
+                path="/*"
+                element={
+                  <Container>
+                    <ErrorPage />
+                  </Container>
+                }
+              />
+            </Routes>
+          </AnimatePresence>
+        </AppContextProvider>
+      </main>
+    </>
+  );
+}
+
+export default App;
